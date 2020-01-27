31a3808facd6a155d271f63d31f0e0980a08f40c
MtMr732 <tbencun156@gmail.com>
2020-01-27 11:27:28 +0900

    fix index.php

--- html/index.php
+++ html/index.php
@@ -1,3 +1,4 @@
+<?php $name="本村"?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,32 +8,9 @@
   <title>Document</title>
 </head>
 <body>
-  <?php 
-    $url="https://api.gnavi.co.jp/RestSearchAPI/v3/" ;
-    $keyid="158bc4d31377b81469b8194028dbef45";
-    // 検索キーワード
-    $name=urlencode($_GET['q']);
-    // $url=$url.'?keyid='.$keyid.'&name='.$name;
-    // エンドポイントでヒットしたデータを代入
-    $data=simplexml_load_file($url);
-  ?>
-  <form action="index.php" method="GET">
-    <input type="text" name="q">
-    <input type="submit" value="検索">
-  </form>
-
-<div id="result">
-  <div>検索結果</div>
-  <?php echo $_GET['q']; ?>
-  <?php 
-    #foreach($data->name as $name){
-      echo $name;
-    #}
-    echo "<pre>";
-    var_dump($name);
-    echo "</pre>";
-
-  ?>
-</div>
+  <div class="container">
+    <h1>Fix_phpfile</h1>
+    <?php echo $name;?>
+  </div>
 </body>
 </html>
