36d004d0f08c4494bb9ed8840c958e369d416cec
MtMr732 <tbencun156@gmail.com>
2020-01-28 13:55:55 +0900

    add webapi.php

---
+++ html/webapi.php
@@ -0,0 +1,53 @@
+<?php 
+  if(!empty($_GET['q'])){
+    $url="https://api.gnavi.co.jp/RestSearchAPI/v3/";
+    $keyid="158bc4d31377b81469b8194028dbef45";
+    $url=$url."?keyid=".$keyid."&name=". urlencode($_GET['q']);
+    $data=file_get_contents($url);
+    $data_array= json_decode($data,true);
+
+    $results=$data_array["rest"];
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container">
+    <h1>ぐるなびAPI改</h1>
+    <form action="" method="GET">
+      <input type="text" name="q">
+      <input type="submit" value="検索">
+    </form>
+    <?php 
+      // ↓デバッグ用のデータ
+      // echo $data_array;
+      // print_r($data_array);
+      // echo count($results);
+      for($i=0;$i<count($results);$i++){
+        echo '<br/>';
+        echo $results[$i]["name"];
+        echo '<br/>';
+        echo '<a href="'.$results[$i]["url"].'">'.$results[$i]["name"].'"のURL"</a>';
+        echo '<br/>';
+        echo '<img src="'.$results[$i]["image_url"]["shop_image1"].'" alt="">' ;
+        echo '<br/>';
+        echo $results[$i]["address"];
+        echo '<br/>';
+        echo $results[$i]["tel"];
+        echo '<br/>';
+      }
+    ?>
+  </div>
+</body>
+</html>
+
+
+
+
+
